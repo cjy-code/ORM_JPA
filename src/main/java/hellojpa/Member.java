fc54713822d50fb87b426a2a9386c45f7842dda2
jychoi <ggozxc@gmail.com>
2022-10-17 23:42:52 +0900

    JPA practice column mapping

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,15 +1,28 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
     @Id
     private Long id;
+    @Column(name= "name")
     private String name;
-//Getter, Setter ...
 
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private  RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private  Date lastModifiedDate;
+
+    @Lob
+    private String descriptionl;
     public Member(){
 
     }
